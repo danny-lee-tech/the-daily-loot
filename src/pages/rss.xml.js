030ed6328493515b5949ba8b8bc69675decb4ac6
Danny Lee <danny.lee.tech@protonmail.com>
2025-08-30 18:34:57 -0400

    Update rss.xml with post items

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,19 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+const posts = await getCollection("posts");
+const filteredPosts = posts.filter(post => !post.data.draft);
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: filteredPosts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
+			pubDate: post.data.date,
+			categories: post.data.tags,
 		})),
 	});
 }
